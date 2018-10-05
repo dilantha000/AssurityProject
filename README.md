fe7f234929d0c48d31e635cd113edbde8debf79c
Dilantha Ariyapala <dilantha.ariyapala@ebuilder.com>
2018-10-05 16:40:34 +0530

    Adding testNG xml

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 
 ## description
 
-what this is doing...
+Purpose of this testcase's are to verify Acceptance criterias are returns the correct values.
+
+Name  = "Carbon credits"
+CanRelist = true
+The Promotions element with Name = "Gallery" has a Description that contains the text "2x larger image"
 
 
 ## Prerequisits
 
 - git
-- java
-- maven
+- java version 1.8
+- maven 3.5.0
+
+## Dependencies
+
+- RestAssured
+- JsonPath
+- TestNG
 
 ## How to run
-test
 
-Run <> as a test ng ...
+Run <> as a testNG ...
 
-
+Generate testNG xml
+Run through maven commands in console. (mvn clean test) and out put will be recorderd to test-output folder.
--- src/test/java/getRequest/ValidateAcceptanceCriteria.java
+++ src/test/java/getRequest/ValidateAcceptanceCriteria.java
@@ -1,6 +1,8 @@
 package getRequest;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,15 +20,19 @@ public class ValidateAcceptanceCriteria {
 	String Name;
 	boolean CanRelist;
 	
+	
 	@BeforeClass
 	public void setupMethod(){
+		
+		//Service response is assigned to variable resp. 
 		resp=RestAssured.get("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false");
 	}
-		
+	
+	/*This test case is to verify whether above API is up and running*/ 
 	@Test(priority = 0)
 	public void verifyResponsecode()
 	{
-			
+		//Verify whether response status code is 200 (SUCCESS).	
 	    int code= resp.getStatusCode();
 	    System.out.println("Status code"+code);
 	    Assert.assertEquals(code, 200); 
@@ -50,6 +56,10 @@ public class ValidateAcceptanceCriteria {
 		System.out.println("Name ="+ Name);
 	}
 	
+	/**
+	 * 
+	 * what is this test case ?
+	 */
 	@Test(priority = 2)
 	public void findCanRelist()
 	{
@@ -60,17 +70,23 @@ public class ValidateAcceptanceCriteria {
 
 
 	@Test(priority = 3)
-	public void findDesc()
+	public void findDescInGallryItem()
 	{
+	
 	String strRes = resp.asString();
 	
 	boolean flag = false;
 	
-	List<HashMap<Integer, HashMap<String,String>>> promo = JsonPath.from(strRes).getList("Promotions");
+	//Getting Promotions JSON object array into a Java List<HashMap>. 
+	List<HashMap> promo = JsonPath.from(strRes).getList("Promotions");
 	
+	//Iterating Promotions list.
 	for(HashMap obj : promo){
-					
+		
+		//Identifying the expected object (Gallery).
 		if(obj.get("Name").equals("Gallery")) {
+			
+			//Getting Gallery element's description through JsonPath get method.
 			String desc = (String) obj.get("Description");
 			
 			if(desc.contains("2x larger image")) {
@@ -80,16 +96,17 @@ public class ValidateAcceptanceCriteria {
 		}
 						
 	}
-		
+	
 	if(flag==true)
 	{
-		System.out.println("The Promotions element with Name \"Gallery\" has a Description that contains the text \"2x larger image\"");
+		System.out.println("The Promotions element with Name = \"Gallery\" has a Description that contains the text \"2x larger image\"");
 	}
 	else
 	{
 		System.out.println("The Promotions element with Name = \"Gallery\" has not a Description that contains the text \"2x larger image\"");
 	}
 	
+	//Asserting whether the flag is true.
 	Assert.assertEquals(flag, true);
 	
 	}
